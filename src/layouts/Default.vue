34e8705a453d4da80744d3a68c1d2081395a7c61
Steve Maciaszek <steve.maciaszek@aurea.com>
2020-10-14 06:32:12 -0700

    initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>eVents</v-toolbar-title>
+
+      <v-text-field
+        placeholder="Placeholder"
+        class="ml-8"
+        style="max-width: 300px"
+        prepend-inner-icon="mdi-magnify"
+        outlined
+        rounded
+        dense
+        hide-details
+      />
+      <v-spacer />
+      <v-btn>Create Event</v-btn>
+    </v-app-bar>
+    <p>{{ $static.metadata.siteDescription }}</p>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteDescription
+  }
+}
+</static-query>
+
+<style>
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <v-container>
+      <v-row>
+        <v-col sm="6" offset-sm="3">
+          <v-tabs v-model="tab" grow>
+            <v-tab>Item One</v-tab>
+            <v-tab>Item Two</v-tab>
+            <v-tab>Item Three</v-tab>
+          </v-tabs>
+        </v-col>
+      </v-row>
+    </v-container>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
